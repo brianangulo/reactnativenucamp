3bf6337f74b5e7f466f46884af46995a878a8375
Brian Angulo <brianangulo96@gmail.com>
2021-04-18 23:50:43 -0400

    Updated redux actions, comment reducer

--- redux/comments.js
+++ redux/comments.js
@@ -8,6 +8,9 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload };
 
+    case ActionTypes.ADD_COMMENT:
+      return { ...state, errMess: null, [action.payload.id]: state.comments.length + 1, comments: [...state.comments, action.payload] }
+
     default:
       return state;
   }
