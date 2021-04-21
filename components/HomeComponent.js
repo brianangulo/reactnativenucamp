d825d3131bd9fb5738cb0503c31cf787c7e53abb
Brian Angulo <brianangulo96@gmail.com>
2021-04-20 22:30:59 -0400

    Added animations to home component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, Animated } from "react-native";
 import { Card } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
 import { PROMOTIONS } from "../shared/promotions";
@@ -47,10 +47,23 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: CAMPSITES,
-      promotions: PROMOTIONS,
-      partners: PARTNERS,
+     scaleValue: new Animated.Value(0),
     };
+  }
+
+  animate() {
+    Animated.timing(
+      this.state.scaleValue,
+      {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true,
+      }
+    ).start();
+  }
+
+  componentDidMount() {
+    this.animate();
   }
 
   static navigationOptions = {
@@ -59,7 +72,9 @@ class Home extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <Animated.ScrollView
+        style={{ transform: [{ scale: this.state.scaleValue }] }}
+      >
         <RenderItem
           item={
             this.props.campsites.campsites.filter(
@@ -87,7 +102,7 @@ class Home extends Component {
           isLoading={this.props.partners.isLoading}
           errMess={this.props.partners.errMess}
         />
-      </ScrollView>
+      </Animated.ScrollView>
     );
   }
 }
