3d8867e9e2c7b1d5581dca8cc555af0271ece8f8
Brian Angulo <brianangulo96@gmail.com>
2021-05-01 09:01:27 -0400

    Added native share option

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -8,7 +8,8 @@ import {
   Button,
   StyleSheet,
   Alert,
-  PanResponder
+  PanResponder,
+  Share
 } from "react-native";
 import { Card, Icon, Input, Rating } from "react-native-elements";
 import { connect } from "react-redux";
@@ -71,14 +72,27 @@ function RenderCampsite(props) {
     }
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title: title,
+        message: `${title}: ${message} ${url}`,
+        url: url,
+      },
+      {
+        dialogTitle: "Share " + title,
+      }
+    );
+  };
+
   if (campsite) {
     return (
-      <Animatable.View 
-      animation="fadeInDown" 
-      duration={2000} 
-      delay={1000}
-      ref={view}
-      {...panResponder.panHandlers}
+      <Animatable.View
+        animation="fadeInDown"
+        duration={2000}
+        delay={1000}
+        ref={view}
+        {...panResponder.panHandlers}
       >
         <Card
           featuredTitle={campsite.name}
@@ -106,6 +120,20 @@ function RenderCampsite(props) {
               raised
               reverse
               onPress={() => props.onShowModal()}
+            />
+            <Icon
+              name={"share"}
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() =>
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
