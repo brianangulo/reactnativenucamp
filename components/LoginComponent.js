987f808916e2300b988be192a29c456705bb5b98
Brian Angulo <brianangulo96@gmail.com>
2021-05-01 11:21:24 -0400

    Workshop completed

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { baseUrl } from "../shared/baseUrl";
+import * as ImageManipulator from "expo-image-manipulator";
 
 class LoginTab extends Component {
   constructor(props) {
@@ -161,10 +162,31 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
       }
     }
   };
+
+   processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(imgUri, [{resize:{width: 400}}], {format: "png"});
+    console.log(processedImage);
+    this.setState({ imageUrl: processedImage.uri });
+  }
+
+  getImageFromGallery = async () => {
+    const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    if (cameraRollPermissions.status === "granted") {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage);
+        this.processImage(capturedImage.uri);
+      }
+    }
+  }
 
   handleRegister() {
     console.log(JSON.stringify(this.state));
@@ -194,6 +216,7 @@ class RegisterTab extends Component {
               style={styles.image}
             />
             <Button title="Camera" onPress={this.getImageFromCamera} />
+            <Button title="Gallery" onPress={this.getImageFromGallery} />
           </View>
           <Input
             placeholder="Username"
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -338,18 +338,21 @@ class Main extends Component {
     this.props.fetchPromotions();
     this.props.fetchPartners();
 
-    NetInfo.fetch().then((connectionInfo) => {
-      Platform.OS === "ios"
-        ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
-        : ToastAndroid.show(
-            "Initial Network Connectivity Type: " + connectionInfo.type,
-            ToastAndroid.LONG
-          );
-    });
+    this.showNetInfo();
 
     this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       this.handleConnectivityChange(connectionInfo);
     });
+  }
+
+  showNetInfo = async () => {
+    const connectionInfo = await NetInfo.fetch();
+      Platform.OS === "ios"
+      ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+      : ToastAndroid.show(
+          "Initial Network Connectivity Type: " + connectionInfo.type,
+          ToastAndroid.LONG
+        )
   }
 
   componentWillUnmount() {
